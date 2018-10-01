23ec6585f7b0784c840c84663aa360be917d05c5
anxu829 <40457335+anxu829@users.noreply.github.com>
2018-10-01 21:54:57 +0800

    Update longestParlindrome.py

--- offer/longestParlindrome.py
+++ offer/longestParlindrome.py
@@ -1,18 +1,29 @@
-string = 'abcba'
+class Solution:
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        string = 'abcba'
 
 
+        string = s
 
-lenOfString = len(string)
-# 注意，每次只需要位置最后一列的数组
-mask = []
-max_ = 0
-for col in range(len(string)):
-    for row in range(0,col + 1):
-        print(row, col)
-        if row == col:
-            mask.append( string[col] == string[row] )
-        if col - row == 1:
-            mask[col -1 ] = string[col] == string[row]
-        if col - row > 1 :
-            mask[row]  = mask[row + 1] and string[row] == string[col]
-    max_ =len(mask) - mask.index(True)  if len(mask) - mask.index(True) > max_ else max_
+        maxstring = ''
+        lenOfString = len(string)
+        # 注意，每次只需要位置最后一列的数组
+        mask = []
+        max_ = 0
+        for col in range(len(string)):
+            for row in range(0,col + 1):
+                # print(row, col)
+                if row == col:
+                    mask.append( string[col] == string[row] )
+                if col - row == 1:
+                    mask[col -1 ] = string[col] == string[row]
+                if col - row > 1 :
+                    mask[row]  = mask[row + 1] and string[row] == string[col]
+            if len(mask) - mask.index(True) > max_ :
+                max_ =len(mask) - mask.index(True) 
+                maxstring = string[mask.index(True): col+1]
+        return maxstring
