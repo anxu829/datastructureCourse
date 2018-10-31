c6e664c7cecb96250519fc1a886dbe899f3ef642
anxu829 <anxu829@gmail.com>
2018-10-31 13:40:50 +0800

    add file

---
+++ notebook/栈和队列/特殊栈的应用/剑指offer_包含min的栈_递减栈.py
@@ -0,0 +1,45 @@
+# -*- coding:utf-8 -*-
+
+# 定义栈的数据结构，请在该类型中实现一个能够得到栈中所含最小元素的min函数（时间复杂度应为O（1））。
+
+# 本题的核心是维护一个递减栈
+
+class Solution:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+    def push(self, node):
+        # write code here
+        self.stack.append(node)
+        if len(self.min_stack) == 0 :
+            # 若辅助栈内容为空， 则 对辅助栈进行压栈
+            self.min_stack.append(node)
+        else:
+            # 否则压入栈顶元素和node的较小值
+            self.min_stack.append(min(node,self.min_stack[-1]))
+            
+    def pop(self):
+        if len(self.stack) != 0 :
+            self.stack.pop()
+            self.min_stack.pop()
+    def top(self):
+        return self.stack[-1]
+    def min(self):
+        return self.min_stack[-1]
+
+if __name__ == '__main__':
+    
+    # case 1 ["PSH3","MIN","PSH4","MIN","PSH2","MIN","PSH3","MIN","POP","MIN","POP","MIN","POP","MIN","PSH0","MIN"]
+    temp = Solution()
+    temp.push(3)
+    temp.min()
+    temp.push(4)
+    temp.min()
+    temp.push(2)
+    temp.min()
+    temp.push(3)
+    temp.min()
+    temp.pop()
+    temp.min()
+    temp.pop()
+    temp.min()
--- offer/temp.py
+++ offer/temp.py
@@ -1,46 +1,45 @@
 # -*- coding:utf-8 -*-
 
+# 定义栈的数据结构，请在该类型中实现一个能够得到栈中所含最小元素的min函数（时间复杂度应为O（1））。
 
-# 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字，
-# 例如，如果输入如下4 X 4矩阵： 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 
-# 则依次打印出数字1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10.
-
-
-# 【错误】忘记加入对 rowE ， colE ， rowS ， colS 的状态更新
-# 【错误】 通不过 [[1],[2],[3],[4],[5]]
+# 本题的核心是维护一个递减栈
 
 class Solution:
-    # matrix类型为二维列表，需要返回列表
-    def printMatrix(self, matrix):
-        res = []
-        rowE = len(matrix) - 1 
-        colE = len(matrix[0]) - 1
-        rowS = 0
-        colS = 0
-        while(colS <= colE or rowS <= rowE):
-            # 首先总是可以打印第一行的
-            for  col in range(colS,colE+1):
-                res.append(matrix[rowS][col])
-            # 然后判断是否走第二段：
-            if rowE - rowS >=2 :
-                for row in range(rowS+1,rowE):
-                    res.append(matrix[row][colE])
-            # 判断是否要走第三段
-            if rowE != rowS:
-                for col in reversed(range(colS,colE+1)):
-                    res.append(matrix[rowE][col])
-            # 判断是否要走第四段：
-            if rowE - rowS >=2  and colE != colS:
-                for row in reversed(range(rowS+1,rowE)):
-                    res.append(matrix[row][colS])
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+    def push(self, node):
+        # write code here
+        self.stack.append(node)
+        if not self.min_stack :
+            # 若辅助栈内容为空， 则 对辅助栈进行压栈
+            self.min_stack.append(node)
+        else:
+            # 否则压入栈顶元素和node的较小值
+            self.min_stack.append(min(node,self.min_stack[-1]))
+            
+    def pop(self):
+        if not self.stack:
+            self.stack.pop()
+            self.min_stack.pop()
+    def top(self):
+        return self.stack[-1]
+    def min(self):
+        return self.min_stack[-1]
 
-            # 更新状态
-            rowS = rowS + 1 ; colS = colS + 1 ; rowE = rowE -1 ; colE = colE - 1;
-        return res
 if __name__ == '__main__':
-    matrix = [[1],[2],[3],[4],[5]]
-    res = Solution().printMatrix(matrix)
-    print(res)
-
-
-
+    
+    # case 1 ["PSH3","MIN","PSH4","MIN","PSH2","MIN","PSH3","MIN","POP","MIN","POP","MIN","POP","MIN","PSH0","MIN"]
+    temp = Solution()
+    temp.push(3)
+    temp.min()
+    temp.push(4)
+    temp.min()
+    temp.push(2)
+    temp.min()
+    temp.push(3)
+    temp.min()
+    temp.pop()
+    temp.min()
+    temp.pop()
+    temp.min()
